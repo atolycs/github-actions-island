1a3f4e1f565d19e03079c62d1dc3e29accdeefe0
Atolycs <dummy.github.1945+atolycs@gmail.com>
2022-12-15 14:20:48 +0900

    [actions] Build Container test

--- .github/workflows/container/entrypoint.sh
+++ .github/workflows/container/entrypoint.sh
@@ -17,3 +17,11 @@ if [ -f "${PACK_NAME}*.pkg.tar.zst" ];then
     ls -lsa *.pkg.tar.zst
     echo "==========================="
 fi
+
+_cmd="$1"
+_pkg_name="$2"
+
+
+case $1 in;
+  build) __build_packs "$2"
+  
