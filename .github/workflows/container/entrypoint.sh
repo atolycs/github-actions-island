7a2962c98f13eb1234dd182fd4a01beed09f4ef5
Atolycs <dummy.github.1945+atolycs@gmail.com>
2022-12-23 11:37:15 +0900

    [build-package] add Update list system

--- .github/workflows/container/entrypoint.sh
+++ .github/workflows/container/entrypoint.sh
@@ -4,24 +4,31 @@ if [ -d "/github" ];then
     sudo chown -R build /github/workspace /github/home
 fi
 
-export MAKEFLAGS=-j$(nproc)
+__build_packs() {
+  export MAKEFLAGS=-j$(nproc)
+  cd workspace
+  namcap PKGBUILD
+  updpkgsums
+  makepkg -fC --syncdeps --noconfirm
+  if [ -f "${PACK_NAME}*.pkg.tar.zst" ];then
+      echo "==========================="
+      echo "Package Created:"
+      ls -lsa *.pkg.tar.zst
+      echo "==========================="
+  fi
+}
 
-cd ${PACK_NAME}
-namcap PKGBUILD
-updpkgsums
-makepkg -fC --syncdeps --noconfirm
+__list_update() {
+  echo ${GPG_SIGN_KEY} | base64 -d | gpg --import
+  cd workspace
+  
 
-if [ -f "${PACK_NAME}*.pkg.tar.zst" ];then
-    echo "==========================="
-    echo "Package Created:"
-    ls -lsa *.pkg.tar.zst
-    echo "==========================="
-fi
+}
+
 
 _cmd="$1"
-_pkg_name="$2"
-
 
 case $1 in;
-  build) __build_packs "$2"
-  
+  build) __build_packs;;
+  repolist) __list_update;;  
+esac  
